8965b939162ca7f1032ee83e1085404f88c3909f
Mark Plant <gothicsyn@live.co.uk>
2015-07-03 10:20:59 +0100

    Starting point of Space Survival Game, a full version of the package will be required to actually build the game out or even access most of the asset packages and design packages. The art assets are held on team systems.

---
+++ Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,61 @@
+﻿/// <summary>
+/// Door Behaviour Script
+/// M A Plant
+/// 02.07.2015
+/// 
+/// Controller for Animation Delay of the Doors at each section inter change.
+/// </summary>
+
+using UnityEngine;
+using System.Collections;
+
+public class DoorBehaviour : MonoBehaviour {
+	
+	public  bool playAnim ;
+	public int _timer = 10;
+	public int time;
+	public int increment = 1;
+	public TextMesh timer;
+	public Animation move;
+
+	void Start(){
+
+		playAnim = true;
+		StartCoroutine (countdown());		// This starts the Timer Co-Routine
+	}
+	
+	void Update(){
+		
+		if(playAnim){
+			
+			playAnim = false;
+			StartCoroutine(Wait());			// This starts the Animation Wait Co-Routine
+			
+		}
+	}
+
+	//This Co-Routine Controls the Animation Delay 
+	
+	public  IEnumerator Wait(){
+		
+		yield return new WaitForSeconds(_timer);
+		move.Play("DoorClose 1");
+	}
+
+
+	// This Co-Routine Controls the counter
+
+	IEnumerator countdown()
+	{
+		while (time >= 0)
+		{
+			yield return new WaitForSeconds(1);
+			
+			timer.text = time.ToString();
+			
+			time -= increment;
+		}
+		
+		timer.text = "Door Sealed";
+	}
+}

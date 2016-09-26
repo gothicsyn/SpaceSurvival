5c54665d08d670dcdf852e214a46551ac2cbc9d6
Mark Plant <gothicsyn@live.co.uk>
2016-09-26 19:39:05 +0100

    WeaponsUpdate
    added in the weapon system from ZAMN it still requires some work though
    so may end up rebuilding from scratch. But also using the Unity Lighting
    demo as a scene for the moment.

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour {
+	public float timer = 3f;
+	public float speed = 4f;
+
+	public Score score;
+
+	void Awake () {
+		score = GameObject.FindGameObjectWithTag ("Score").GetComponent<Score> ();
+	}
+
+	// Use this for initialization
+	void Start () {
+		Invoke ("Die", timer);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * speed * Time.deltaTime;
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.tag == "Enemy") {
+			CancelInvoke ();
+			Destroy (other.gameObject);
+			Debug.Log("Point Call + 100 Zombie");
+			score.Add (100);
+			Die ();
+		}
+	}
+
+	void Die () {
+		Destroy(gameObject);
+	}
+}
